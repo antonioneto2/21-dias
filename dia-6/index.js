ce0ce81c17246c6837a24f7a773ce6ac02efa8b6
antonioneto2 <antonio.nog28@gmail.com>
2024-06-17 11:01:18 -0300

    exercicio 4, forma menos eficiente

--- dia-6/index.js
+++ dia-6/index.js
@@ -1,3 +1,18 @@
-for (let i = 50; i>= 0; i-=5){
-    console.log(i)
+let num = parseInt(prompt('Digite um número inteiro e positivo: '))
+let seg = num + 1
+let ter = num + 2
+
+console.log("Número " + num + ":")
+for(let i = 0; i <=10; i++){
+    console.log(num + " x " + i + " = " + (num*i))
+}
+
+console.log("Número " + seg + ":")
+for(let i = 0; i<=10; i++){
+    console.log(seg + " x " + i + " = " + (seg*i))
+}
+
+console.log("Número " + ter + ":")
+for(let i = 0; i<=10; i++){ 
+    console.log(ter + " x " + i + " = " + (ter*i))
 }
