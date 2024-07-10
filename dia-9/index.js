01364b66ce05e9097b0fefeeb43e87a28cc908d2
antonioneto2 <antonio.nog28@gmail.com>
2024-07-10 15:10:29 -0300

    finalizando exercicio 1

--- dia-9/index.js
+++ dia-9/index.js
@@ -4,6 +4,8 @@ let pesoUsuario = parseInt(prompt("Informe seu peso: "))
 let alturaUsuario = Number(prompt("Informe sua altura: "))
 let profUsuario = prompt("Informe sua profissão: ")
 
+
+do {
 console.log("Olá " + nomeUsuario + ", você tem " + idadeUsuario + " anos, é "+ profUsuario + ", tem " + alturaUsuario + "M de altura e pesa " + pesoUsuario + "kg.")
 
 if(idadeUsuario >= 18){
@@ -11,3 +13,35 @@ if(idadeUsuario >= 18){
 }else{
     console.log("Sem gelada pra você.")
 }
+
+let imc = 0
+
+imc = pesoUsuario / (alturaUsuario*alturaUsuario)
+
+if (imc < 18.5){
+    console.log("Você está muito magro.")
+}else if(imc >= 18.5 && imc <= 24.9){
+    console.log("Você está no peso ideal.")
+}else if(imc >= 24.9 && imc <= 30){
+    console.log("Você está com sobrepeso.")
+}else if (imc > 30){
+    console.log("Você está com obsidade")
+}
+let anoUsuario = 2024
+anoUsuario -=idadeUsuario
+
+console.log("Você nasceu em " + anoUsuario)
+
+let idadeAtual = 0
+
+for (let index = anoUsuario; index < 2025 ; index++) {
+    console.log(index + " - " + idadeAtual + " anos de idade")
+    idadeAtual++
+       
+}
+
+let continuar = prompt("Deseja inserir os dados novamente? (s/n)")
+
+} while (continuar == "s");
+
+
