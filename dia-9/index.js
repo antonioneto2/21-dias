509759424b3d3359363310be6db427396c1da220
antonioneto2 <antonio.nog28@gmail.com>
2024-07-11 09:25:10 -0300

    exercicio 2, menos eficiente

--- dia-9/index.js
+++ dia-9/index.js
@@ -1,47 +1,35 @@
-let nomeUsuario = prompt("Informe seu nome: ")
-let idadeUsuario = parseInt(prompt("Informe sua idade: "))
-let pesoUsuario = parseInt(prompt("Informe seu peso: "))
-let alturaUsuario = Number(prompt("Informe sua altura: "))
-let profUsuario = prompt("Informe sua profissão: ")
+let nomeUsuario 
+let idadeUsuario = 0 
+let salarioAtual = 0
+let continuar 
+
+do { 
+
+nomeUsuario = prompt("Informe seu nome:")
+idadeUsuario = parseInt(prompt("Informe sua idade:"))
+salarioAtual = Number(prompt("Informe seu salário atual:"))
+
+continuar = prompt("Os dados estão corretos? (s/n)")
+
+}while(continuar == "n")
+
+console.log("Nome: " + nomeUsuario)
+console.log("Idade: " + idadeUsuario + " anos")
+console.log("Salário atual: R$ " + salarioAtual)
+
+console.log("Previsão de salário para os próximos 10 anos")
+
+let anoAtual = 2024
+let aumentoSalario = 0
+let porcSalario = 0.015
+aumentoSalario = salarioAtual * porcSalario
+salarioAtual = salarioAtual + aumentoSalario
+
+for(let i = anoAtual+1; i <= anoAtual+10; i++ ){
+    console.log(i + " - " + salarioAtual)
+    porcSalario = porcSalario * 2
+    aumentoSalario = salarioAtual * porcSalario
+    salarioAtual = salarioAtual + aumentoSalario
 
 
-do {
-console.log("Olá " + nomeUsuario + ", você tem " + idadeUsuario + " anos, é "+ profUsuario + ", tem " + alturaUsuario + "M de altura e pesa " + pesoUsuario + "kg.")
-
-if(idadeUsuario >= 18){
-    console.log("Está liberado para tomar umas geladas.")
-}else{
-    console.log("Sem gelada pra você.")
 }
-
-let imc = 0
-
-imc = pesoUsuario / (alturaUsuario*alturaUsuario)
-
-if (imc < 18.5){
-    console.log("Você está muito magro.")
-}else if(imc >= 18.5 && imc <= 24.9){
-    console.log("Você está no peso ideal.")
-}else if(imc >= 24.9 && imc <= 30){
-    console.log("Você está com sobrepeso.")
-}else if (imc > 30){
-    console.log("Você está com obsidade")
-}
-let anoUsuario = 2024
-anoUsuario -=idadeUsuario
-
-console.log("Você nasceu em " + anoUsuario)
-
-let idadeAtual = 0
-
-for (let index = anoUsuario; index < 2025 ; index++) {
-    console.log(index + " - " + idadeAtual + " anos de idade")
-    idadeAtual++
-       
-}
-
-let continuar = prompt("Deseja inserir os dados novamente? (s/n)")
-
-} while (continuar == "s");
-
-
