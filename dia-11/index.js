62f1a1556901cba0522d6a18a5a998ba72fcacea
antonioneto2 <antonio.nog28@gmail.com>
2024-12-05 14:22:32 -0300

    Questão 1

--- dia-11/index.js
+++ dia-11/index.js
@@ -1,2 +1,29 @@
-let nome = [];
-let nota = [];
+let alunos = []
+let notas = []
+let continuar = true 
+let contador = 0
+
+while (continuar) {
+    let nome = prompt("Digite seu nome " + (contador + 1) + "º aluno")
+    let nota = parseInt(prompt("Digite a nota do aluno"))
+    alunos[contador] = nome
+    notas[contador] = nota
+    contador++
+    let resposta = prompt("Deseja inserir informções de outro aluno? (s/n)")
+    if (resposta == "n")
+        continuar = false
+}
+
+console.log("Nota dos alunos: ")
+for (let i = 0; i < alunos .length; i++){
+    console.log(alunos[i] + " - " + notas[i])
+}
+
+let somaNotas = 0
+for(let i = 0; i < notas.length; i++){
+    somaNotas += notas[i]
+}
+
+let media = somaNotas / alunos.length
+console.log("A soma das notas foi: " + somaNotas)
+console.log("A média geral da turma foi: " + media)
